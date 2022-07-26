b70a32e351b53756e9846c68637fbd9233996a7c
ArasModir <109582452+ArasModir@users.noreply.github.com>
2022-07-26 10:11:47 +0200

    use the File system class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,34 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>My Blog</title>
     <link rel="stylesheet" href="/app.css">
 
     </script>
-  </head>
-  <body>
-
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-            <p>
-                The year 1886 is regarded as the birth year of the car when German inventor Carl Benz patented his Benz Patent-Motorwagen.[3][4][5] Cars became widely available during the 20th century. One of the first cars affordable by the masses was the 1908 Model T, an American car manufactured by the Ford Motor Company. Cars were rapidly adopted in the US, where they replaced animal-drawn carriages and carts.[6] In Europe and other parts of the world, demand for automobiles did not increase until after World War II.[6] The car is considered an essential part of the developed economy.
-            </p>
-    </article>
-
-    <article>
-      <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-          <p>
-              The year 1886 is regarded as the birth year of the car when German inventor Carl Benz patented his Benz Patent-Motorwagen.[3][4][5] Cars became widely available during the 20th century. One of the first cars affordable by the masses was the 1908 Model T, an American car manufactured by the Ford Motor Company. Cars were rapidly adopted in the US, where they replaced animal-drawn carriages and carts.[6] In Europe and other parts of the world, demand for automobiles did not increase until after World War II.[6] The car is considered an essential part of the developed economy.
-          </p>
-    </article>
-
-    <article>
-      <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-          <p>
-              The year 1886 is regarded as the birth year of the car when German inventor Carl Benz patented his Benz Patent-Motorwagen.[3][4][5] Cars became widely available during the 20th century. One of the first cars affordable by the masses was the 1908 Model T, an American car manufactured by the Ford Motor Company. Cars were rapidly adopted in the US, where they replaced animal-drawn carriages and carts.[6] In Europe and other parts of the world, demand for automobiles did not increase until after World War II.[6] The car is considered an essential part of the developed economy.
-          </p>
-    </article>
-
-  </body>
+</head>
+    <body>
+        <?php foreach ($posts as $post) : ?>
+            <article>
+                <?= $post; ?>
+            </article>
+        <?php endforeach; ?>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,20 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
 
-    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect('/');
-//        abort(404);
-    }
-
-    $post = cache()->remember("post.{$slug}", now()->addMinute(20),
-        fn() => file_get_contents($path));
-
     return view('post', [
-        'post' => $post
+        'post' => Post::find($slug)
     ]);
-})->where('post', '[A-z_-]+');
+})->where('post', '[A-z_\-]+');
