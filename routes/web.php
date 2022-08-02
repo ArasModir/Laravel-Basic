3645da86df5ac95435d2c27319c91b31d6d5b2a0
ArasModir <109582452+ArasModir@users.noreply.github.com>
2022-08-02 12:46:20 +0200

    Make a post model and migration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Models\Post;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,28 +16,14 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
 
-    // The above and the down are equal
-
-//    $posts = array_map(function ($file) {
-//        $document = YamlFrontMatter::parseFile($file);
-//
-//        return new Post(
-//            $document-> title,
-//            $document-> excerpt,
-//            $document-> date,
-//            $document-> body(),
-//            $document-> slug
-//        );
-//    },$files);
-
     return view('posts', [
         'posts' => Post::all()
     ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function ($id) {
 
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Post::findOrFail($id)
     ]);
 });
